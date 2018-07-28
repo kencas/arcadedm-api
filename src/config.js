50330f9f42add774a01ebf12adb4287f165b9983
kencas <appcalypsounltd@gmail.com>
2018-07-28 15:55:56 +0100

    hi

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //"database" : "mongodb://localhost:27017/9cab", 
-    "database" : "mongodb://appcalypso:appgreatreal45@ds163610.mlab.com:63610/appcalypso",
-    "port" : process.env.PORT || 30000,
-    "secretKey" : "66&&&888^^TTggg4$$$55g"
-}
